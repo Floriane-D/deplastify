09aa77e50f08d08b50d471fe5a10de0c503002a0
Lucas Martins <lucaslealm@gmail.com>
2019-03-18 18:57:43 -0300

    search working on homepage

---
+++ app/javascript/home.js
@@ -0,0 +1,12 @@
+const locationLink = document.querySelector('#location');
+const searchAddress = document.querySelector('#search_address');
+
+locationLink.addEventListener('click', () => {
+  navigator.geolocation.getCurrentPosition((data) => {
+    var searchForm = document.querySelector('#search_form');
+    var lat = data.coords.latitude;
+    var lon = data.coords.longitude;
+    searchAddress.value = `${lat},${lon}`
+    searchForm.submit();
+  });
+});
