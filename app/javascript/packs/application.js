cd518d7788dc47e2c3a4d37b46b81fdecbb1dd55
Lucas Martins <lucaslealm@gmail.com>
2019-03-18 18:00:05 -0300

    search done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,9 @@
 import "bootstrap";
-
+import "results";
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { initMapbox } from '../plugins/init_mapbox';
+
+import { initAutocomplete } from '../plugins/init_autocomplete';
+initAutocomplete();
 
 initMapbox();
