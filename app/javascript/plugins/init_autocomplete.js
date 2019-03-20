6d21fa36ed111a6f73f096fef2cef64da3c83ccd
Floriane Didier <florianedidier@gmail.com>
2019-03-20 15:45:54 -0300

    autocomplete works everywhere

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -1,10 +1,18 @@
 import places from 'places.js';
 
-const initAutocomplete = () => {
-  const addressInput = document.getElementById("search_address");
+let initAutocomplete = () => {
+  let addressInput = document.querySelector('.address-auto');
   if (addressInput) {
     places({ container: addressInput });
   }
 };
 
+// const initAutocompleteBis = () => {
+//   const addressInputBis = document.querySelector('.address-auto');
+//   if (addressInputBis) {
+//     places({ container: addressInputBis });
+//   }
+// };
+
 export { initAutocomplete };
+// export { initAutocompleteBis };
