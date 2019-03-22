a58ae6b0798c1c0670dffedd1e3e01c61ca0e3b2
Lucas Martins <lucaslealm@gmail.com>
2019-03-22 14:14:23 -0300

    map working

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -8,21 +8,35 @@ const buildMap = (coords) => {
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v10',
     center: coords,
-    zoom: 13 // starting zoom
+    zoom: 13
   });
 };
 
 const addMarkersToMap = (map, markers, coords) => {
   markers.forEach((marker) => {
-    const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
+  const storeElement = document.createElement('div');
+  storeElement.className = 'marker';
+  storeElement.style.backgroundImage = `url('https://img.icons8.com/ultraviolet/100/000000/marker.png')`;
+  storeElement.style.backgroundSize = 'contain';
+  storeElement.style.width = '42px';
+  storeElement.style.height = '42px';
 
-    new mapboxgl.Marker()
-      .setLngLat([ marker.lng, marker.lat ])
-      .setPopup(popup)
-      .addTo(map);
+  const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
+
+  new mapboxgl.Marker(storeElement)
+    .setLngLat([ marker.lng, marker.lat ])
+    .setPopup(popup)
+    .addTo(map);
   });
 
-  const userMarker = new mapboxgl.Marker()
+  const element = document.createElement('div');
+  element.className = 'marker';
+  element.style.backgroundImage = `url('https://img.icons8.com/office/100/000000/marker.png')`;
+  element.style.backgroundSize = 'contain';
+  element.style.width = '42px';
+  element.style.height = '42px';
+
+  const userMarker = new mapboxgl.Marker(element)
     .setLngLat(coords)
     .addTo(map);
 };
