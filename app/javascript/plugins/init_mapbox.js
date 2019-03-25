b416ee4d6b50303537d022612dc43fc5d54e2e40
Lucas Martins <lucaslealm@gmail.com>
2019-03-25 17:04:42 -0300

    map fixes

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -28,6 +28,30 @@ const addMarkersToMap = (map, markers, coords) => {
   });
 };
 
+const addMarkersToMapWithoutCoords = (map, markers) => {
+  markers.forEach((marker) => {
+  const storeElement = document.createElement('div');
+  storeElement.className = 'marker';
+  storeElement.style.backgroundImage = `url('https://img.icons8.com/dusk/100/000000/turtle.png')`;
+  storeElement.style.backgroundSize = 'contain';
+  storeElement.style.width = '35px';
+  storeElement.style.height = '35px';
+
+  const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
+
+  new mapboxgl.Marker(storeElement)
+    .setLngLat([ marker.lng, marker.lat ])
+    .setPopup(popup)
+    .addTo(map);
+  });
+};
+
+const fitMapToMarkers = (map, markers) => {
+  const bounds = new mapboxgl.LngLatBounds();
+  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+  map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
+};
+
 const addGeolocationControl = (map, markers) => {
   const geoControl = new mapboxgl.GeolocateControl({
     positionOptions: {
@@ -46,13 +70,28 @@ const addGeolocationControl = (map, markers) => {
 
 const initMapbox = () => {
   if (mapElement) {
-    navigator.geolocation.getCurrentPosition((data) => {
-      const coords = [data.coords.longitude, data.coords.latitude];
+    navigator.geolocation.getCurrentPosition(function(position) {
+    // navigator.geolocation.getCurrentPosition((data) => {
+      const coords = [position.coords.longitude, position.coords.latitude];
       const map = buildMap(coords);
       const markers = JSON.parse(mapElement.dataset.markers);
       addMarkersToMap(map, markers, coords);
       addGeolocationControl(map, markers);
-    });
+  },
+  function(error) {
+    if (error.code == error.PERMISSION_DENIED) {
+      mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+      const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v10'
+      });
+      const markers = JSON.parse(mapElement.dataset.markers);
+      if (markers) {
+      addMarkersToMapWithoutCoords(map, markers);
+      fitMapToMarkers(map, markers);
+    }}
+    // });
+  });
   }
 };
 
