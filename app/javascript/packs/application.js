de1fcefc536400083ddb7b940746b5bb3582621c
Lucas Martins <lucaslealm@gmail.com>
2019-03-26 13:14:41 -0300

    name changed to first_name on voucher details

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,7 @@ import "results";
 import "home";
 
 import 'mapbox-gl/dist/mapbox-gl.css';
+
 import { initMapbox } from '../plugins/init_mapbox';
 
 import { initAutocomplete } from '../plugins/init_autocomplete';
